713b40c7a840eb77026dd00909203bc55a6986bd
COBI-98 <tkdgus968@naver.com>
2023-12-07 15:05:59 +0900

    refactor: 에러메시지 검증 상수 통일

--- src/main/java/racingcar/message/RacingCarErrorMessage.java
+++ src/main/java/racingcar/message/RacingCarErrorMessage.java
@@ -1,12 +1,12 @@
 package racingcar.message;
 
 public enum RacingCarErrorMessage {
-    NOT_CAR_NAME_LENGTH_CORRECT("자동차이름은 1자리이상 5자 이하만 가능합니다."),
-    NOT_NATURAL_NUMBER_VALIDATE("이동할 횟수는 자연수여야 한다."),
-    DUPLICATED_CAR_NAME("자동차 이름은 중복으로 사용될 수 없습니다."),
-    NOT_MIN_CAR_COUNT("자동차는 최소 2대 이상 생성해야 합니다."),
-    CAR_LIST_EMPTY("차량 리스트가 비었습니다."),
-    DUPLICATED_DELIMITER("중복으로 구분자를 사용할 수 없습니다.");
+    INVALID_CAR_NAME_LENGTH("자동차이름은 1자리이상 5자 이하만 가능합니다."),
+    INVALID_TRY_COUNT_NATURAL_NUMBER("이동할 횟수는 자연수여야 한다."),
+    INVALID_DUPLICATION_CAR_NAME("자동차 이름은 중복으로 사용될 수 없습니다."),
+    INVALID_MINIMUM_CARS_COUNT("자동차는 최소 2대 이상 생성해야 합니다."),
+    INVALID_CARS_EMPTY("차량 리스트가 비었습니다."),
+    INVALID_DUPLICATED_DELIMITER("중복으로 구분자를 사용할 수 없습니다.");
 
     String message;
 
